6b9a459a08d25934e02e64ca8cbb4d837b706614
tamuno777 <davisbeulah07@gmail.com>
2023-07-15 12:37:49 -0700

    welll

--- src/sections/searchroom.jsx
+++ src/sections/searchroom.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Col ,Row} from 'react-bootstrap'
-import image1 from '../assets/imagea.png'
-import image2 from '../assets/imageb.png'
-import image3 from '../assets/imagec.png'
-import image4 from '../assets/imaged.png'
+// import image1 from '../assets/imagea.png'
+// import image2 from '../assets/imagebbb.svg'
+// import image3 from '../assets/imagec.png'
+// import image4 from '../assets/imaged.png'
 
 export default function Searchroom() {
   return (
@@ -11,12 +11,19 @@ export default function Searchroom() {
       <Row className='px-5 mb-5'>
         <Col className='me-3 room-img '>
           <div className="d-flex">
-            <img src={image1} alt="" className='long'/>
-            <img src={image2} alt="" className='short'/>
+            {/* <img src={image1} alt="" className=''/> */}
+            <p className='long text-center' style={{fontWeight:'bold'}}>Flexible Leases</p>
+            {/* <img src={image2} alt="" className=''/> */}
+            <p className='short text-center' style={{fontWeight:'bold'}}>7-Day Happiness Guaranteed </p>
+
           </div>
           <div className="d-flex">
-            <img src={image3} alt="" className='long-short'/>
-            <img src={image4} alt="" className='wide'/>
+            {/* <img src={image3} alt="" className='long-short'/> */}
+            <p className='long-short text-center' style={{fontWeight:'bold'}}>Flexible Leases</p>
+
+            {/* <img src={image4} alt="" className='wide'/> */}
+            <p className='wide text-center' style={{fontWeight:'bold'}}>Flexible Leases</p>
+
           </div>
         
         </Col>
